8e126644fe9f4d430fcb5e93c37fe858180aa605
Surya Kanigolla <suryakanigolla@gmail.com>
2021-09-05 14:40:22 +0530

    Added Client Build

--- server/app.js
+++ server/app.js
@@ -18,6 +18,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./build")));
 
 export default app;
